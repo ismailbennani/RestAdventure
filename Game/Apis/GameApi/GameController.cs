6dd6f93994d9ea7f23390bcc9883163ebf9023b6
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2024-05-03 21:13:34 +0200

    add game controller exposing settings

---
+++ Game/Apis/GameApi/GameController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NSwag.Annotations;
+using RestAdventure.Game.Authentication;
+using RestAdventure.Game.Settings;
+
+namespace RestAdventure.Game.Apis.GameApi;
+
+[Route("game")]
+[ApiController]
+[Authorize(AuthenticationSchemes = GameApiAuthenticationOptions.AuthenticationScheme)]
+[GameApi]
+[OpenApiTag("Game")]
+public class GameController : ControllerBase
+{
+    readonly IOptions<GameSettings> _gameSettings;
+
+    public GameController(IOptions<GameSettings> gameSettings)
+    {
+        _gameSettings = gameSettings;
+    }
+
+    /// <summary>
+    ///     Get settings
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("settings")]
+    public GameSettings GetGameSettings() => _gameSettings.Value;
+}
