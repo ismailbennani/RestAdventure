f04c809445990d9b46fac435088ed5cee3b9cfcb
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2024-05-09 14:40:31 +0200

    introduce immediate action

---
+++ Core/Actions/ImmediateAction.cs
@@ -0,0 +1,18 @@
+﻿using RestAdventure.Core.Entities.Characters;
+
+namespace RestAdventure.Core.Actions;
+
+public abstract class ImmediateAction : Action
+{
+    protected ImmediateAction(string name) : base(name)
+    {
+    }
+
+    public override bool IsOver(GameState state, Character character) => true;
+
+    protected override sealed Task OnStartAsync(GameState state, Character character) => Task.CompletedTask;
+    protected override sealed Task OnTickAsync(GameState state, Character character) => PerformAsync(state, character);
+    protected override sealed Task OnEndAsync(GameState state, Character character) => Task.CompletedTask;
+
+    protected abstract Task PerformAsync(GameState state, Character character);
+}
--- Core/Maps/MoveAction.cs
+++ Core/Maps/MoveAction.cs
@@ -1,11 +1,11 @@
-﻿using RestAdventure.Core.Entities.Characters;
+﻿using RestAdventure.Core.Actions;
+using RestAdventure.Core.Entities.Characters;
 using RestAdventure.Core.Maps.Locations;
 using RestAdventure.Kernel.Errors;
-using Action = RestAdventure.Core.Actions.Action;
 
 namespace RestAdventure.Core.Maps;
 
-public class MoveAction : Action
+public class MoveAction : ImmediateAction
 {
     public MoveAction(Location location) : base("move")
     {
@@ -15,8 +15,5 @@ public class MoveAction : Action
     public Location Location { get; }
 
     protected override Maybe CanPerformInternal(GameState state, Character character) => character.Movement.CanMoveTo(state, Location);
-
-    public override bool IsOver(GameState state, Character character) => true;
-
-    protected override async Task<Maybe> OnStartAsync(GameState state, Character character) => await character.Movement.MoveToAsync(state, Location);
+    protected override async Task PerformAsync(GameState state, Character character) => await character.Movement.MoveToAsync(state, Location);
 }
