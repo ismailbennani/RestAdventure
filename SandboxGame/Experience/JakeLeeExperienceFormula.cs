f557fe0bf014dbf7193dcec02dd3ce890ac21994
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2024-05-12 13:41:23 +0200

    change job experience formula

--- SandboxGame/Experience/JakeLeeExperienceFormula.cs
+++ SandboxGame/Experience/JakeLeeExperienceFormula.cs
@@ -15,6 +15,20 @@ public class JakeLeeExperienceFormula : ExperienceFormula
     public double X { get; }
     public double Y { get; }
 
-    public override int ComputeExperience(int level) => (int)Math.Pow(level / X, Y);
-    public override int ComputeLevel(int experience) => (int)(X * Math.Pow(experience, 1.0 / Y));
+    public override int ComputeExperience(int level) =>
+        Y switch
+        {
+            1 => (int)(level / X),
+            2 => (int)(level * level / (X * X)),
+            3 => (int)(level * level * level / (X * X * X)),
+            _ => (int)Math.Pow(level / X, Y)
+        };
+
+    public override int ComputeLevel(int experience) =>
+        Y switch
+        {
+            1 => (int)(X * experience),
+            2 => (int)Math.Sqrt(X * experience),
+            _ => (int)(X * Math.Pow(experience, 1.0 / Y))
+        };
 }
--- SandboxGame/Experience/KnownExperienceFormulas.cs
+++ SandboxGame/Experience/KnownExperienceFormulas.cs
@@ -2,6 +2,6 @@
 
 public static class KnownExperienceFormulas
 {
-    public static readonly ExperienceFormula JobExperienceFormula = new JakeLeeExperienceFormula(0.07, 2);
+    public static readonly ExperienceFormula JobExperienceFormula = new JakeLeeExperienceFormula(0.3, 2);
     public static readonly IReadOnlyList<int> JobLevelCaps1To50 = JobExperienceFormula.ComputeLevelCaps(1, 50);
 }
